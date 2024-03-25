068944415c3e01e9c93cee2f5d748bb1b33c1b16
준영 <128689935+ljy21123@users.noreply.github.com>
2024-03-25 20:02:18 +0900

    알고리즘 수업 - 알고리즘의 수행 시간 2

---
+++ Bronze/Bronze_IV/p24263.java
@@ -0,0 +1,23 @@
+package Bronze.Bronze_IV;
+
+import java.util.Scanner;
+
+// 알고리즘 수업 - 알고리즘의 수행 시간 2
+public class p24263 {
+
+    public static void main(String[] args) {
+
+        // O(n)에 대한 문제이다.
+        // 시간복잡도, 빅오표기법에 대해 이해해야한다.
+        // https://blog.chulgil.me/algorithm/
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(n);
+        System.out.println(1);
+
+    }
+
+
+}
