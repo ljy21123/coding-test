adc474169b271f9084af9532c774c6535629e58f
준영 <128689935+ljy21123@users.noreply.github.com>
2024-04-03 21:07:51 +0900

    블랙잭

---
+++ Bronze/Bronze_II/p2798.java
@@ -0,0 +1,46 @@
+package Bronze.Bronze_II;
+
+import java.util.*;
+
+// 블랙잭
+public class p2798 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt(); // 카드의 갯수
+        int M = sc.nextInt(); // 목표 점수
+        int diff = 0;
+
+        int[] num = new int[N];
+        List<Integer> sum = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            num[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < N - 2; i++) {
+            for (int j = i + 1; j < N - 1; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    int hap = num[i] + num[j] + num[k];
+                    if (hap <= M) {
+                        sum.add(hap);
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < sum.size(); i++) {
+            diff = M - sum.get(i);
+            result.add(diff);
+        }
+
+        System.out.println(M - Collections.min(result));
+
+        sc.close();
+
+    }
+
+}
