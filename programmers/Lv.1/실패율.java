99bb18b341c5591955e3d3bea6bd69db8dd57ef7
준영 <128689935+ljy21123@users.noreply.github.com>
2025-03-04 01:06:07 +0900

    Lv.1

--- programmers/Lv.1/실패율.java
+++ programmers/Lv.1/실패율.java
@@ -1,18 +1,9 @@
 import java.util.*;
 
 /*
- * 스테이지 번호와 실패율을 짝으로 저장하는 방법, HashMap에 대해 알게되었다.
- * HashMap을 사용하는건 간단하였지만 return할 때 여러 조건이 붙어 복잡했다.
- *
- * .entrySet()
- * > 해당 HashMap의 모든 쌍(엔트리)을 가져온다.
- *
- * .sorted((o1, o2) -> Double.compare(o2.getValue(), o1.getValue()))
- * > 스트림에 있는 엔트리들을 value 값을 기준으로 내림차순으로 정렬한다.
- *
- * .mapToInt(HashMap.Entry::getKey)
- * > 각 엔트리의 key만 추출하여 IntStream으로 변환한다.
- */
+* 스테이지 번호와 실패율을 짝으로 저장하는 방법, HashMap에 대해 알게되었다.
+* HashMap을 사용하는건 간단하였지만 return할 때 여러 조건이 붙어 복잡했다.
+* */
 
 class Solution {
     public int[] solution(int N, int[] stages) {
@@ -49,7 +40,18 @@ class Solution {
 
         }
 
-        // value를 기준으로 내림차순 정렬 후 key만 배열에 담아 반환
+        /*
+        * value를 기준으로 내림차순 정렬 후 key만 배열에 담아 반환
+        *
+        * .entrySet()
+        * > 해당 HashMap의 모든 쌍(엔트리)을 가져온다.
+        *
+        * .sorted((o1, o2) -> Double.compare(o2.getValue(), o1.getValue()))
+        * > 스트림에 있는 엔트리들을 value 값을 기준으로 내림차순으로 정렬한다.
+        *
+        * .mapToInt(HashMap.Entry::getKey)
+        * > 각 엔트리의 key만 추출하여 IntStream으로 변환한다.
+        * */
         return answer.entrySet().stream().sorted((o1, o2) -> Double.compare(o2.getValue(), o1.getValue()))
                 .mapToInt(HashMap.Entry::getKey).toArray();
 
